531ec01632e599f82f3c0cbe2062f96b1ad6cbcd
jrnibs <johnnibato13@gmail.com>
2025-03-13 16:53:46 +0800

    lesson

---
+++ function.php
@@ -0,0 +1,10 @@
+<?php
+function writemessage() {
+    echo "WELCOME TO MY PAGE HEELOOO";
+    echo "<br> hellow im jOHn";
+}
+
+function setHeight($minHeight = 50){
+    echo "THe Height is " . $minHeight ."<br>";
+}
+?>
