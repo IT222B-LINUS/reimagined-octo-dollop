f687938443fed3723b93384bec50b1af3b5e45f8
jrnibs <johnnibato13@gmail.com>
2025-03-13 18:22:59 +0800

    lesson update

--- function.php
+++ function.php
@@ -4,7 +4,23 @@ function writemessage() {
     echo "<br> hellow im jOHn";
 }
 
+function familyName($fname){
+    echo "$fname Refsnes. <br>";
+}
+
+
+function familybday($fname,$year){
+    echo "$fname Refsnes. Born in $year <br>";
+}
+    
+
 function setHeight($minHeight = 50){
     echo "THe Height is " . $minHeight ."<br>";
 }
+
+function sum($x,$y){
+    $z = $x + $y;
+    return $z;
+}
+    
 ?>
--- index.php
+++ index.php
@@ -10,9 +10,34 @@
     include 'function.php';
 
     writemessage();
+
+    echo "<hr>";
     
-    setHeight("350");
+    familyName("Jani");
+    familyName("Hege");
+    familyName("Stale");
+    familyName("Kai Jim");
+    familyName("Borge");
+
+    echo "<hr>";
+    
+    familybday("Hege","1975");
+    familybday("Stale","1978");
+    familybday("Kai Jim","1983");
+
+    echo "<hr>";
+
+    setHeight(350);
     setHeight();
+    setHeight(135);
+    setHeight(80);
+
+
+    echo "<hr>";
+
+    echo "5 + 10 =".sum(5,10)."<br>";
+    echo "7 + 13 =".sum(7,13)."<br>";
+    echo "2 + 4 =".sum(2,4)."<br>";
 
     ?>
 </body>
